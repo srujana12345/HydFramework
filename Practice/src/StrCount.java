3e75ebf908aea6a5df621d97b6b7b7b6bb3068cd
DOVE <DOVE@ms>
2018-11-19 13:59:43 +0530

    yesssss

---
+++ Practice/src/StrCount.java
@@ -0,0 +1,25 @@
+
+public class StrCount
+{
+ public static void main(String args[])
+ {
+	String str1="Qspiders";
+	 String str2="";
+	 System.out.println(str1.compareTo(str2));
+	 
+	/* String str="Qspiders";
+	 int count=0;
+	 while(true)
+	 {
+		 try
+		 {
+			 str.charAt(++count);
+		 }
+		 catch(Throwable t)
+		 {
+			 break;
+		 }
+	 }
+	 System.out.println(count);*/
+ }
+}
