538aa7b6c8bf936b7821c55ce728f2217943bef6
srujana12345 <45061867+srujana12345@users.noreply.github.com>
2018-11-19 15:46:19 +0530

    Update StrCount.java

--- Practice/src/StrCount.java
+++ Practice/src/StrCount.java
@@ -3,11 +3,11 @@ public class StrCount
 {
  public static void main(String args[])
  {
-	String str1="Qspiders";
+	/*String str1="Qspiders";
 	 String str2="";
-	 System.out.println(str1.compareTo(str2));
+	 System.out.println(str1.compareTo(str2));*/
 	 
-	/* String str="Qspiders";
+	String str="Qspiders";
 	 int count=0;
 	 while(true)
 	 {
@@ -20,6 +20,6 @@ public class StrCount
 			 break;
 		 }
 	 }
-	 System.out.println(count);*/
+	 System.out.println(count);
  }
 }
